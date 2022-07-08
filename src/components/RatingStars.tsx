c566a34eb795d7dfcdba4d60eb0038adda5451fd
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2022-07-08 04:51:15 +0200

    Use custom icon for empty star

--- src/components/RatingStars.tsx
+++ src/components/RatingStars.tsx
@@ -1,7 +1,16 @@
 import Rating from '@mui/material/Rating'
+import StarIcon from '@mui/icons-material/Star'
 
 export default function RatingStars({ value }: { value: number }) {
+  console.log(value)
+
   return (
-    <Rating name="half-rating-read" value={value} precision={0.5} readOnly />
+    <Rating
+      name="half-rating-read"
+      value={value}
+      precision={0.5}
+      emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+      readOnly
+    />
   )
 }
